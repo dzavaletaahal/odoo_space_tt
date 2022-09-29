f7304fb7e11db8a1747676485dfb7503859e1088
Duvan <dzavaleta@ahalconsultores.com>
2022-09-29 17:33:21 +0000

    [ADD] ORM avanzado

--- odoo_space_tt/models/spaceship.py
+++ odoo_space_tt/models/spaceship.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
 
 class Spaceship(models.Model):
     
@@ -9,8 +10,17 @@ class Spaceship(models.Model):
 
     name = fields.Char(string='Spaceship name', required=True)
     ship_dimensions = fields.Char(string='Dimensions')
+    ship_lenght = fields.Integer()
+    ship_width = fields.Integer()
     ship_type = fields.Char(string='Ship Type')
     num_passengers = fields.Integer(string='Number of passengers')
     fuel_type = fields.Char(string='Type of fuel')
     active = fields.Boolean(string="Active")
+    
+    
+    @api.constrains('ship_lenght','ship_width')
+    def _check_additional_fee(self):
+        for record in self:
+            if record.ship_width > record.ship_lenght:
+                raise ValidationError(_('Width can not be bigger than lenght' )) 
     
